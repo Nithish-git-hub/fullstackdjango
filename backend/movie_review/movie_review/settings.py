143e1d5d8b348a6eb35f4794e6c22a8bb748173f
nithish-n2002 <n6302016824@gmail.com>
2025-11-17 10:57:37 +0530

    vite1

--- backend/movie_review/movie_review/settings.py
+++ backend/movie_review/movie_review/settings.py
@@ -163,9 +163,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+# STATIC_ROOT=BASE_DIR/'staticfiles'
+# STATICFILES_DIRS=[]
 
 
 # Default primary key field type
