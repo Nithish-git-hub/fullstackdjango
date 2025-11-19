af0188d64ea854099bd84b610c4a39b8a89e3152
nithish-n2002 <n6302016824@gmail.com>
2025-11-20 00:12:58 +0530

    cloud 2

--- frontend/frontend_app/src/utils/axios.js
+++ frontend/frontend_app/src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:8000/api/",
-  baseURL: "https://fullstackdjango-4.onrender.com/api/",
+  baseURL: "https://fullstackdjango-5.onrender.com/api/",
 });
 
 instance.interceptors.request.use((config) => {
